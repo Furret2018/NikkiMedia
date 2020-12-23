7d6ebba7e5288e5f0744e642f9f9a5d4826649d9
AuraSF <370402166@qq.com>
2020-12-23 18:38:34 +0800

    Create CMusic.c

---
+++ SRC/CMusic.c
@@ -0,0 +1,92 @@
+#include <windows.h>
+// https://blog.csdn.net/qq_34333801/article/details/80726911
+// https://blog.csdn.net/liuzhihan209/article/details/8994584
+
+// https://blog.csdn.net/u011555996/article/details/77945081
+// https://wenku.baidu.com/view/1de44a03ad02de80d5d84004.html
+// https://www.cnblogs.com/mfrank/p/6166254.html
+
+// https://www.jb51.net/article/127920.htm
+// https://cloud.tencent.com/developer/article/1016630
+
+
+// https://blog.csdn.net/qq_30460949/article/details/90543803
+// https://jingyan.baidu.com/article/e8cdb32bf36a3637052bad26.html
+// https://blog.csdn.net/slinace/article/details/79762667
+// https://www.zhihu.com/question/328827430/answer/719189392
+// https://blog.csdn.net/c80486/article/details/44528829
+// https://bbs.csdn.net/topics/360003586
+// Low
+
+#define L1 262
+#define L2 294
+#define L3 330
+#define L4 349
+#define L5 392
+#define L6 440
+#define L7 493
+
+// High
+#define N1 532
+#define N2 588
+#define N3 660
+#define N4 698
+#define N5 784
+#define N6 880
+#define N7 988
+
+#define H1 1046
+#define H2 1175
+#define H3 1319
+#define H4 1397
+#define H5 1568
+#define H6 1760
+#define H7 1976
+
+
+#define A 1600
+#define B 800
+#define C 400
+#define D 200
+#define E 100
+#define F 50
+#define G 25
+
+#define unsigned int uint
+
+// #define Beep(x1, x2) x#x1#x2 
+
+void alert(int);
+void sung();
+
+
+main(){
+    sung();
+	//x#N1#C;
+}
+
+
+void alert(int i)
+{
+    while (i) Beep(1000, 1000), i--;
+}
+
+void sung()
+{
+	Beep(N1, C); Beep(N2, C); Beep(N3, C); Beep(N1, C);
+	Beep(N1, C); Beep(N2, C); Beep(N3, C); Beep(N1, C);
+	Beep(N3, C); Beep(N4, C); Beep(N5, C); Beep(0, C);
+	Beep(N3, C); Beep(N4, C); Beep(N5, C); Beep(0, C);
+	
+	Beep(N5, D+E); Beep(N6, E); Beep(N5, D+E); Beep(N4, E); Beep(N3, C); Beep(N1, C);
+	Beep(N5, D+E); Beep(N6, E); Beep(N5, D+E); Beep(N4, E); Beep(N3, C); Beep(N1, C);
+	
+	Beep(N2, C); Beep(N5, C); Beep(N1, C); Beep(0, C); 
+	Beep(N2, C); Beep(N5, C); Beep(N1, C); Beep(0, C);  
+	
+}
+
+//https://open.163.com/newview/movie/free?pid=M84LGFEVN&mid=M8543OVJD#from=article
+
+//http://renjian.163.com/20/1222/11/FUES5J5C000199ET.html#f=post1603_tab_news
+
